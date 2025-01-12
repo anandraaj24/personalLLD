7ec04f7c256a8abbce2939c045dba09a557a0385
anand346 <rajanand9039@gmail.com>
2025-01-12 20:53:56 +0530

    Added live lectures counting in lecture counter script

--- PHPTools/lecture-counter.js
+++ PHPTools/lecture-counter.js
@@ -1,21 +1,22 @@
 var courseItemsDiv = document.querySelector('.courseItems');
 var allCoursesList = courseItemsDiv.querySelectorAll('.courseItem.courseItem__id');
 var allCourseDetails = [];
+
 allCoursesList.forEach(item => {
     var courseDetails = {
         courseId: item.getAttribute("data-id"),
         courseTitle: item.getAttribute("data-title")
     }
     allCourseDetails.push(courseDetails);
-    
-})
+});
 
 allCourseDetails.forEach(item => {
     var courseDiv = document.querySelector('div[data-id="'+item.courseId+'"]');
     var courseTitle = item.courseTitle;
-    var lecturesDiv = courseDiv.querySelectorAll('.courseSubItem.courseItem__id[data-type="video"]').length+1;
-    console.log("Subject Name: " + courseTitle + ", Total Lectures: " + lecturesDiv);
-})
+    // Select both 'video' and 'liveclass' types
+    var lecturesDiv = courseDiv.querySelectorAll('.courseSubItem.courseItem__id[data-type="video"], .courseSubItem.courseItem__id[data-type="liveclass"]').length;
+    console.log("Subject Name: " + courseTitle + ", Total Lectures (including Live Classes): " + lecturesDiv);
+});
 
 
 
